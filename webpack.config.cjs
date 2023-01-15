96052c2e1e3d6fcaf5b47e7da57afe8678151cd5
TarquiniusMajor <barribassov@yandex.ru>
2023-01-15 21:48:12 +0300

    fix attempt 3

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
